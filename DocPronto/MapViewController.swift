432e1ce4562c8aad9bf6c2ee8a8fafef483b2ec6
Bobby Ren <bobbyren@gmail.com>
2015-08-02 18:48:04 -0400

    added reverse geocoder; added request button

--- DocPronto/MapViewController.swift
+++ DocPronto/MapViewController.swift
@@ -12,9 +12,11 @@ import GoogleMaps
 class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate {
 
     @IBOutlet var mapView: GMSMapView!
+    @IBOutlet var iconLocation: UIImageView!
     let locationManager = CLLocationManager()
-    var iconImage: UIImage?
     var currentLocation: CLLocation?
+    
+    @IBOutlet var buttonRequest: UIButton!
     
     // address view
     @IBOutlet var viewAddress: UIView!
@@ -25,6 +27,8 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.buttonRequest.enabled = false
+        
         locationManager.delegate = self
         
         if (locationManager.respondsToSelector("requestWhenInUseAuthorization")) {
@@ -33,15 +37,20 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
         else {
             locationManager.startUpdatingLocation()
         }
-        
-        self.iconImage = UIImage(named: "iconLocation")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
-        
+
         self.mapView.myLocationEnabled = true
+        self.iconLocation.layer.zPosition = 1
+
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.Done, target: self, action: "close")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func close() {
+        self.navigationController!.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
     }
     
     // MARK: - CLLocationManagerDelegate
@@ -61,19 +70,32 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         if let location = locations.first as? CLLocation {
-            mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 17, bearing: 0, viewingAngle: 0)
             locationManager.stopUpdatingLocation()
             
             self.currentLocation = location
+            self.updateMapToCurrentLocation()
+            self.enableRequest()
         }
     }
 
+    func updateMapToCurrentLocation() {
+        var zoom = self.mapView.camera.zoom
+        if zoom < 12 {
+            zoom = 17
+        }
+        self.mapView.camera = GMSCameraPosition(target: self.currentLocation!.coordinate, zoom: zoom, bearing: 0, viewingAngle: 0)
+    }
+    
+    func enableRequest() {
+        self.buttonRequest.enabled = true
+        self.buttonRequest.layer.zPosition = 1
+    }
     // MARK: - GMSMapView  delegate
     func didTapMyLocationButtonForMapView(mapView: GMSMapView!) -> Bool {
         self.view.endEditing(true)
 
         if self.currentLocation != nil {
-            self.mapView.camera = GMSCameraPosition(target: self.currentLocation!.coordinate, zoom: self.mapView.camera.zoom, bearing: 0, viewingAngle: 0)
+            self.updateMapToCurrentLocation()
         }
         locationManager.startUpdatingLocation()
         return false
@@ -81,6 +103,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
     
     func mapView(mapView: GMSMapView!, idleAtCameraPosition position: GMSCameraPosition!) {
         self.currentLocation = CLLocation(latitude: position.target.latitude, longitude: position.target.longitude)
+        self.enableRequest()
     }
     
     // MARK: Location search
@@ -97,13 +120,10 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
             else {
                 println("result: \(results)")
                 if let placemarks: [CLPlacemark] = results as? [CLPlacemark] {
-                    if let placemark: CLPlacemark = placemarks.last as CLPlacemark! {
+                    if let placemark: CLPlacemark = placemarks.first as CLPlacemark! {
                         self.currentLocation = CLLocation(latitude: placemark.location.coordinate.latitude, longitude: placemark.location.coordinate.longitude)
-                        var zoom = self.mapView.camera.zoom
-                        if zoom < 12 {
-                            zoom = 17
-                        }
-                        self.mapView.camera = GMSCameraPosition(target: self.currentLocation!.coordinate, zoom: zoom, bearing: 0, viewingAngle: 0)
+                        self.updateMapToCurrentLocation()
+                        self.enableRequest()
                     }
                 }
             }
@@ -116,6 +136,37 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
         return true
     }
 
+    // MARK: - request
+    @IBAction func didClickRequest(sender: UIButton) {
+        if self.currentLocation != nil {
+            let coder = GMSGeocoder()
+            coder.reverseGeocodeCoordinate(self.currentLocation!.coordinate, completionHandler: { (response, error) -> Void in
+                let gmresponse:GMSReverseGeocodeResponse = response as GMSReverseGeocodeResponse
+                let results: [AnyObject] = gmresponse.results()
+                let addresses: [GMSAddress] = results as! [GMSAddress]
+                let address: GMSAddress = addresses.first!
+                
+                var addressString: String = ""
+                let lines: [String] = address.lines as! [String]
+                for line: String in lines {
+                    addressString = "\(addressString)\n\(line)"
+                }
+                println("Address: \(addressString)")
+                
+                self.confirmRequestForAddress(addressString, coordinate: address.coordinate)
+            })
+        }
+    }
+    
+    func confirmRequestForAddress(addressString: String, coordinate: CLLocationCoordinate2D) {
+        var alert: UIAlertController = UIAlertController(title: "Request doctor?", message: "Do you want to schedule a visit at \(addressString)?", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Pronto!", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+            println("requesting")
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
     func simpleAlert(title: String?, message: String?) {
         var alert: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
         alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.Cancel, handler: nil))
